6378077187ef52125503e18c7991813eb6d9bc43
Panikos <panikos.christofi@gmail.com>
2020-06-21 21:32:39 +0100

    amplify authentication

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -23,4 +24,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);

35eefe2020ccb9f3689fe1979db609df5c7d1b2f
Panikos <panikos.christofi@gmail.com>
2020-06-21 21:48:52 +0100

    updates

---
+++ amplify/backend/function/premon14684932/src/index.js
@@ -0,0 +1,21 @@
+/* Amplify Params - DO NOT EDIT
+	API_PREMON_BLOGTABLE_ARN
+	API_PREMON_BLOGTABLE_NAME
+	API_PREMON_COMMENTTABLE_ARN
+	API_PREMON_COMMENTTABLE_NAME
+	API_PREMON_GRAPHQLAPIENDPOINTOUTPUT
+	API_PREMON_GRAPHQLAPIIDOUTPUT
+	API_PREMON_POSTTABLE_ARN
+	API_PREMON_POSTTABLE_NAME
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
